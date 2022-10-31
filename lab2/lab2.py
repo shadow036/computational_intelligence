06f844b3d9ede718a0e67027b7744890f4474e70
shadow <Christian>
2022-10-31 16:02:47 +0100

    more improvements

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,31 +1,43 @@
-from phases import parent_selection, recombination_plus, recombination_comma, mutation, fitness_function, replacement
-from hyperparameters import N, GENERATIONS
+from phases import parent_selection, recombination, mutation, fitness_function, replacement
 from utilities import problem, hide_fitness
+
+N = 50
+GENERATIONS = 10    # other than the initial one
 
 if __name__ == '__main__':
     input_ = problem(N, seed_=42)
-    total = sum(len(l) for l in input_)
-    e_solutions = input_.copy()
-    current_best = [(None, -total), (None, -total)]
+    worst_case_cost = sum(len(l) for l in input_) - N
+    current_best = [None, None]
     mu = len(input_)
-    offsprings_plus = 10 * mu
-    offsprings_comma = mu
+    offsprings = [mu, 10 * mu]  # lambdas for plus and comma strategy
+
+    # PLUS STRATEGY
+    c_solutions = [[i] for i in input_]
+    e_solutions = fitness_function(c_solutions, worst_case_cost, N)
+    current_best[0] = sorted(e_solutions, key=lambda t: t[1], reverse=True)[0]
     for i in range(GENERATIONS):
-        c_solutions = recombination_plus(e_solutions, offsprings_plus, mu, total) # list => list
-        c_solutions = mutation(e_solutions, input_) # list => list
-        e_solutions = fitness_function(c_solutions, total) # list => (list, fitness)
-        e_solutions = replacement(e_solutions, mu)    # (list, fitness) => (list, fitness)
-        if current_best[0][1] < e_solutions[0][1]:
-            current_best[0] = (e_solutions[0][0], e_solutions[0][1])
-        print(current_best[0][1])
-        e_solutions = hide_fitness(e_solutions)  # (list, fitness) => list
-    # e_solutions = input_.copy()
-    # for i in range(GENERATIONS):
-    #     c_solutions = recombination_comma(e_solutions, OFFSPRINGS_COMMA, total) # list => list
-    #     #c_solutions = mutation(e_solutions, input_) # list => list
-    #     e_solutions = fitness_function(c_solutions, total) # list => (list, fitness)
-    #     e_solutions = artificial_selection(e_solutions, mu)    # (list, fitness) => (list, fitness)
-    #     if current_best[1][1] < e_solutions[0][1]:
-    #         current_best[1] = (e_solutions[0][0], e_solutions[0][1])
-    #     print(current_best[1][1])
-    #     e_solutions = [e_solutions[_][0] for _ in range(len(e_solutions))]  # (list, fitness) => list
+        c_solutions = hide_fitness(e_solutions)
+        for j in range(offsprings[0]):
+            p0, p1 = parent_selection(c_solutions[:mu], worst_case_cost, N)
+            c_solutions.append(recombination(p0, p1))
+        c_solutions = mutation(c_solutions, mu, input_)
+        e_solutions = fitness_function(c_solutions, worst_case_cost, N)
+        e_solutions = replacement(e_solutions, mu)
+        if e_solutions[0][1] > current_best[0][1]:
+            current_best[0] = e_solutions[0]
+    
+    # COMMA STRATEGY
+    c_solutions = [[i] for i in input_]
+    e_solutions = fitness_function(c_solutions, worst_case_cost, N)
+    current_best[1] = sorted(e_solutions, key=lambda t: t[1], reverse=True)[0]
+    for i in range(GENERATIONS):
+        possible_parents = hide_fitness(e_solutions)
+        c_solutions = []
+        for j in range(offsprings[1]):
+            p0, p1 = parent_selection(possible_parents, worst_case_cost, N)
+            c_solutions.append(recombination(p0, p1))
+        c_solutions = mutation(c_solutions, 0, input_)
+        e_solutions = fitness_function(c_solutions, worst_case_cost, N)
+        e_solutions = replacement(e_solutions, mu)
+        if e_solutions[0][1] > current_best[1][1]:
+            current_best[1] = e_solutions[0]
--- lab2/phases.py
+++ lab2/phases.py
@@ -1,65 +1,59 @@
 from random import choice, sample
 import logging
 from math import ceil, floor
-from hyperparameters import PARENT_SELECTION_PARAMETER, INVERSE_MUTATION_RATE, N
 
-def parent_selection(e_solutions, total):
+INVERSE_MUTATION_RATE = 20  # higher value => lower probability of mutations occurring
+PARENT_SELECTION_PARAMETER = 4  # list of elements from which to choose the 2 parents
+
+def parent_selection(e_solutions, worst_case_cost, N):
     parents =   sorted(
                     fitness_function(
-                        sample(e_solutions, PARENT_SELECTION_PARAMETER), total
+                        sample(e_solutions, PARENT_SELECTION_PARAMETER), worst_case_cost, N
                     ), 
                     key=lambda t: t[1], reverse=True
                 )[:2]   # list => (list, fitness)
     return [parents[_][0] for _ in range(2)]    # (list, fitness) => list
 
-def recombination_plus(e_solutions, offsprings, mu, total):
-    c_solutions = e_solutions.copy()
-    for i in range(offsprings):
-        parents = parent_selection(c_solutions[:mu], total)
-        heritage0 = [parents[0]] if type(parents[0][0]) is int else parents[0][:ceil(len(parents[0])/2)]
-        heritage1 = [parents[1]] if type(parents[1][0]) is int else parents[1][floor(len(parents[1])/2):]
-        c_solutions.append(heritage0 + heritage1)
-    return c_solutions
-        
-def recombination_comma(e_solutions, offsprings, total):
-    c_solutions = []
-    for i in range(offsprings):
-        parents = parent_selection(e_solutions, total)
-        heritage0 = [parents[0]] if type(parents[0][0]) is int else parents[0][:ceil(len(parents[0])/2)]
-        heritage1 = [parents[1]] if type(parents[1][0]) is int else parents[1][floor(len(parents[1])/2):]
-        c_solutions.append(heritage0 + heritage1)
-    return c_solutions
+def recombination(p0, p1):
+    h0 = p0[:ceil(len(p0)/2)]
+    h1 = p1[floor(len(p1)/2):]
+    return h0 + h1
     
-def mutation(c_solutions, input_):
-    for index in range(len(c_solutions)):
-        m = choice(range(INVERSE_MUTATION_RATE))
-        if type(c_solutions[index][0]) is int or len(c_solutions[index]) == 1:
-            if m == 1: # switch
+def mutation(c_solutions, mu, input_):
+    for index in range(mu, len(c_solutions)):
+        if len(c_solutions[index]) == 1:
+            m = choice(range(INVERSE_MUTATION_RATE))
+            if m in [1, 2]: 
+                # switch
                 c_solutions[index] = input_[choice(range(len(input_)))]
-            elif m == 2:    # add
+            elif m in [3, 4]:    
+                # add
                 c_solutions[index] = [c_solutions[index], input_[choice(range(len(input_)))]]
         else:
             for index2 in range(len(c_solutions[index])):
-                if m == 0:  # remove
+                m = choice(range(INVERSE_MUTATION_RATE))
+                if m == 0 and c_solutions[index].count(-1) != len(c_solutions[index]) - 1:  
+                    # remove
                     c_solutions[index][index2] = -1
-                elif m == 1: # switch
+                elif m in [1, 2]: 
+                    # switch
                     c_solutions[index][index2] = input_[choice(range(len(input_)))]
-                elif m == 2:    # add
+                elif m in [3, 4]:    
+                    # add
                     c_solutions[index].append(input_[choice(range(len(input_)))])
             c_solutions[index] = [e for e in c_solutions[index] if e != -1]
     return c_solutions
         
-def fitness_function(c_solutions, total):
-    e_solutions = c_solutions.copy()    # in order to make clear the difference between candidate and evaluated solutions
+def fitness_function(c_solutions, worst_case_cost, N):
+    e_solutions = c_solutions.copy()
+    # ^ in order to make clear the difference between candidate and evaluated solutions
     for index in range(len(e_solutions)):
         counters = [0 for _ in range(N)]
-        if type(e_solutions[index][0]) is int:
-            counters = [counters[_]+1 if _ in e_solutions[index] else counters[_] for _ in range(N)]
-        else:
-            for e in e_solutions[index]:
-                counters = [counters[_]+1 if _ in e else counters[_] for _ in range(N)]
-        partial_fitness = sum([1 - counters[_] if counters[_] > 1 else counters[_] for _ in range(N)])
-        offset = (total if counters.count(0) > 0 else 0)
+        for e in e_solutions[index]:
+            counters = [counters[_]+1 if _ in e else counters[_] for _ in range(N)]
+        partial_fitness = sum([1 - counters[_] if counters[_] > 1 
+                               else counters[_] for _ in range(N)])
+        offset = (worst_case_cost if counters.count(0) > 0 else 0)
         e_solutions[index] = (e_solutions[index], partial_fitness - offset)
     return e_solutions
 
